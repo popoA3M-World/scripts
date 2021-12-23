4ff30456d881d34ddeea5575b4d3bef8d3769752
popoAXM <96530473+popoAXM@users.noreply.github.com>
2021-12-23 09:14:24 +0530

    scripts: E kernel script

---
+++ kernel_E.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="Aura-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$HOME/tc/proton-clang"
+DEFCONFIG="holland1_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+	echo "Proton clang not found! Cloning to $TC_DIR..."
+	if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang "$TC_DIR"; then
+		echo "Cloning failed! Aborting..."
+		exit 1
+	fi
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	exit 1
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j"$(nproc --all)" O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+if [ -f "out/arch/arm64/boot/Image.gz" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if ! git clone -q https://github.com/popoASM-World/AnyKernel3 -b aura; then
+		echo -e "\nCloning AnyKernel3 repo failed! Aborting..."
+		exit 1
+	fi
+	cp out/arch/arm64/boot/Image.gz AnyKernel3
+	rm -f ./*zip
+	cd AnyKernel3 || exit
+	rm -rf out/arch/arm64/boot
+	zip -r9 "../$ZIPNAME" ./* -x '*.git*' README.md ./*placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+	curl -F document=@"$ZIPNAME" "https://api.telegram.org/bot1358441517:AAG0GL__2I7s83hXDMtXaeUaQartZEl5xnc/sendDocument" -F chat_id="620358472" -F "parse_mode=Markdown" -F caption="*✅ Build finished after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds*"
+	echo
+else
+	echo -e "\nCompilation failed!"
+fi
