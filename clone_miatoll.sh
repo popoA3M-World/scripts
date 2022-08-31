ecb3aa22a0e3c2bd791b1aa2bee3b04c2c87bec2
popoA3M <pratyayaborborah@gmail.com>
2022-08-31 09:29:41 +0000

    scripts: Add basic cloning script
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>

---
+++ clone_miatoll.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/Spark-Devices/device_xiaomi_sm6250-common
+git clone https://github.com/Spark-Devices/android_device_xiaomi_miatoll
+git clone https://github.com/Spark-Devices/kernel_xiaomi_sm6250 --depth=1
+git clone https://github.com/Spark-Devices/vendor_xiaomi_sm6250-common
+git clone https://github.com/Spark-Devices/vendor_xiaomi_miatoll
