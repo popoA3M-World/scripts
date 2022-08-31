a0dc7d7faec3843095c377b085a50666952d8504
popoA3M <pratyayaborborah@gmail.com>
2022-08-31 09:38:22 +0000

    scripts: Add support cloning E trees
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>

---
+++ clone_E.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/popoA3M/android_device_10or_E device/10or/E
+git clone https://github.com/popoA3M/android_vendor_10or_E vendor/10or
+git clone https://github.com/popoA3M/android_kernel_10or_E --depth=1 kernel/10or/E
