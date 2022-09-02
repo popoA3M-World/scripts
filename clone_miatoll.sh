a2ed98cb6e31cf120446434ff21fbc7dc1fdd1da
Pratyay Borborah <102370202+popoA3M@users.noreply.github.com>
2022-09-02 14:41:57 +0530

    scripts: It's pyro time

--- clone_miatoll.sh
+++ clone_miatoll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/Spark-Devices/device_xiaomi_sm6250-common
+git clone https://github.com/Spark-Devices/device_xiaomi_sm6250-common -b pyro
 git clone https://github.com/Spark-Devices/android_device_xiaomi_miatoll
 git clone https://github.com/Spark-Devices/kernel_xiaomi_sm6250 --depth=1
 git clone https://github.com/Spark-Devices/vendor_xiaomi_sm6250-common
